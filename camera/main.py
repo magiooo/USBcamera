fbf8229c8500c43c962c568f510a736db7579729
yaasann <47031019+yaasann@users.noreply.github.com>
2019-01-25 22:06:55 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, Response
 # from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
 # from processor.face_detector import FaceDetector as VideoCamera
-from processor.person_detector import PersonDetector as VideoCamera
+from processor.person_detector import PersonDetector as WebcamVideoStream
 
 import time
 import threading
 
-video_camera = VideoCamera(flip=False)
+video_camera = WebcamVideoStream(flip=False)
 
 
 app = Flask(__name__)
