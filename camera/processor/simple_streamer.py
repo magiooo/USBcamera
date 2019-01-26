a3e0d08e27fb5587353afc98b2de3498faf42eaa
yaasann <47031019+yaasann@users.noreply.github.com>
2019-01-27 00:04:12 +0900

    Update simple_streamer.py

--- camera/processor/simple_streamer.py
+++ camera/processor/simple_streamer.py
@@ -7,7 +7,7 @@ import cv2
 
 class SimpleStreamer(object):
     def __init__(self, flip = False):
-        self.vs = WidecamVideoCapture(resolution=(400, 304), framerate=3).start()
+        self.vs = /dev/video0(resolution=(400, 304), framerate=3).start()
         self.flip = flip
         time.sleep(2.0)
 
