1bcd6a07715a76dc5867650531c7d1f21aabbb5f
yaasann <47031019+yaasann@users.noreply.github.com>
2019-01-27 23:28:46 +0900

    Update simple_streamer.py

--- camera/processor/simple_streamer.py
+++ camera/processor/simple_streamer.py
@@ -7,27 +7,23 @@ import cv2
 
 class SimpleStreamer(object):
     def __init__(self, flip = False):
-        # Define the codec and create VideoWriter object
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-        # Video Capture
         try:
-            self.vc = cv2.VideoCapture(0)
+            self.vs = PiVideoStream(resolution=(640, 480)).start()
         except:
-            print(self.vc)
+            print(slf.vs)
         self.flip = flip
         time.sleep(2.0)
 
     def __del__(self):
-        self.out.release()
-        self.vc.release()
+        self.vs.stop()
 
     def flip_if_needed(self, frame):
         if self.flip:
-            return np.flip(frame, 0)
+            return np.flip(frame, 1)
         return frame
 
     def get_frame(self):
         frame = self.flip_if_needed(self.vs.read())
         ret, jpeg = cv2.imencode('.jpg', frame)
         return jpeg.tobytes()
+
