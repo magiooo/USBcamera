bb70b2ad9320dfe8b6b84bcaefeb2a9ae03b06b4
yaasann <47031019+yaasann@users.noreply.github.com>
2019-02-06 13:53:08 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -9,10 +9,14 @@ from processor.person_detector import PersonDetector as VideoCamera
 import time
 import threading
 
-video_camera = VideoCamera(flip=True)
+video_camera = VideoCamera(flip=False)
 
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 def gen(camera):
     while True:
@@ -20,15 +24,10 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/')
-def index():
+@app.route('/video_feed')
+def video_feed():
     return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/save')
-def save():
-    while True:
-        video_camera.save_frame()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, threaded=True)
