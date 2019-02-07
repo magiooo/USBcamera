f6cb6aa5bb6d2bcab0d7e3e65f33b90dd698795a
yaasann <47031019+yaasann@users.noreply.github.com>
2019-02-07 14:25:23 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -47,9 +47,9 @@ class PersonDetector(object):
         return image.tobytes()
 
     def process_image(self, frame):
-        frame = imutils.resize(frame, width=300)
+        frame = imutils.resize(frame, width=600)
         (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (150, 150), 127.5)
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (600, 600), 127.5)
         net.setInput(blob)
         detections = net.forward()
 
