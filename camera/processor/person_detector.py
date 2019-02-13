4ebd7fe087557cda58198f647aed5974159b1187
magiooo <45158755+magiooo@users.noreply.github.com>
2019-02-13 11:27:38 +0900

    Update person_detector.py

--- camera/processor/person_detector.py
+++ camera/processor/person_detector.py
@@ -96,7 +96,7 @@ class PersonDetector(object):
         if count > 0:
             print('Count: {}'.format(count))
             elapsed = time.time() - self.last_upload
-            if elapsed > 20:
+            if elapsed > 15:
                 cv2.imwrite('hello.jpg', frame)
                 upload()
                 self.last_upload = time.time()
